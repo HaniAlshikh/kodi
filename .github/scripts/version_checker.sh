ba9e60f24c373a80c483297cc741b9263326b2de
HaniAlshikh <Alshikh.hani@gmail.com>
2021-09-29 02:34:01 +0200

    wrong artifact name for draft version

--- .github/scripts/version_checker.sh
+++ .github/scripts/version_checker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 current=$(awk '/name="H-S Repo" version/ {print $6}' FS='"' $RELEASE/$REPO_FOLDER/addon.xml)
-expected=$(cat release-version.txt)
+expected=$(cat draft-version.txt)
 echo "repo version: $current" && echo "draft version: ${expected:1}"
 if [ "$current" != "${expected:1}" ]; then
   exit 1
