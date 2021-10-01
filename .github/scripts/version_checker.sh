5ea2408a666ae76b56cf1b038b940fe8a0ba2301
HaniAlshikh <Alshikh.hani@gmail.com>
2021-10-01 16:34:36 +0200

    fallty logic

--- .github/scripts/version_checker.sh
+++ .github/scripts/version_checker.sh
@@ -3,5 +3,5 @@
 current=$(awk '/name="H-S Repo" version/ {print $6}' FS='"' $RELEASE/$REPO_FOLDER/addon.xml)
 echo "repo version: $current" && echo "draft version: ${EXCPECTED:1}"
 if [ "$current" != "${EXCPECTED:1}" ]; then
-  echo true
+  echo false
 fi
