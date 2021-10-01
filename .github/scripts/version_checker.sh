96946685a47acddd54c84ea07a813e9344c67156
HaniAlshikh <Alshikh.hani@gmail.com>
2021-10-01 16:17:28 +0200

    using jobs outputs to exchange version

--- .github/scripts/version_checker.sh
+++ .github/scripts/version_checker.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 current=$(awk '/name="H-S Repo" version/ {print $6}' FS='"' $RELEASE/$REPO_FOLDER/addon.xml)
-expected=$(cat draft-version.txt)
-echo "repo version: $current" && echo "draft version: ${expected:1}"
-if [ "$current" != "${expected:1}" ]; then
+#EXCPECTED=$(cat draft-version.txt)
+echo "repo version: $current" && echo "draft version: ${EXCPECTED:1}"
+if [ "$current" != "${EXCPECTED:1}" ]; then
   echo true
 else
   echo false
