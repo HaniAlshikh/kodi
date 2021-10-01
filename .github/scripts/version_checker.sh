e493f7904ce593d2ee933505ef880d8de77a0466
HaniAlshikh <Alshikh.hani@gmail.com>
2021-10-01 16:25:51 +0200

    using outputs instead of artifacts

--- .github/scripts/version_checker.sh
+++ .github/scripts/version_checker.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 current=$(awk '/name="H-S Repo" version/ {print $6}' FS='"' $RELEASE/$REPO_FOLDER/addon.xml)
-#EXCPECTED=$(cat draft-version.txt)
 echo "repo version: $current" && echo "draft version: ${EXCPECTED:1}"
 if [ "$current" != "${EXCPECTED:1}" ]; then
   echo true
